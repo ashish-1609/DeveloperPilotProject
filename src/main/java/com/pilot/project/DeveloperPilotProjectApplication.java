39308a9a5a1db6c689d8800de7e32a418ee45b1d
Ashish <ashish@celerispay.com>
2024-09-16 15:36:20 +0530

    Initial Commit

---
+++ src/main/java/com/pilot/project/DeveloperPilotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pilot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperPilotProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperPilotProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pilot/project/DeveloperPilotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilot.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperPilotProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
