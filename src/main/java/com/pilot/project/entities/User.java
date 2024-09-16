450440848ca67a4af06fb5a2350e4b932fd14986
Ashish <ashish@celerispay.com>
2024-09-16 15:36:33 +0530

    Initial Commit

---
+++ src/main/java/com/pilot/project/entities/User.java
@@ -0,0 +1,21 @@
+package com.pilot.project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String userId;
+    private String name;
+    private String email;
+    private String password;
+}
