13e7d85ee5f750d148686bbd5b16f8f5333bd99e
Ashish <ashish@celerispay.com>
2024-09-24 16:30:53 +0530

    Test cases

---
+++ src/test/java/com/pilot/project/configs/JwtAuthenticationEntryPointTest.java
@@ -0,0 +1,37 @@
+package com.pilot.project.configs;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.AuthenticationException;
+
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class JwtAuthenticationEntryPointTest {
+    @InjectMocks
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private AuthenticationException exception;
+
+    @Test
+    void commence() throws IOException, ServletException {
+        jwtAuthenticationEntryPoint.commence(request, response, exception);
+        verify(response).sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+    }
+
+}
--- src/test/java/com/pilot/project/configs/JwtAuthenticationFilterTest.java
+++ src/test/java/com/pilot/project/configs/JwtAuthenticationFilterTest.java
@@ -7,11 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
