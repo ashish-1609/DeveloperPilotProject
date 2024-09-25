a98ca98a03f72a8ac9736b929d4cc11a42a03a67
Ashish <ashish@celerispay.com>
2024-09-25 16:41:33 +0530

    Fixing Security Errors

--- src/test/java/com/pilot/project/configs/JwtAuthenticationFilterTest.java
+++ src/test/java/com/pilot/project/configs/JwtAuthenticationFilterTest.java
@@ -7,9 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
