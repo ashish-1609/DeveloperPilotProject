5cefcf1e854130d50ee198bacb83caf5b6f78d15
Ashish <ashish@celerispay.com>
2024-09-25 17:25:37 +0530

    Fixing Security Errors

---
+++ src/test/java/com/pilot/project/configs/JwtTokenHelperTest.java
@@ -0,0 +1,68 @@
+package com.pilot.project.configs;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class JwtTokenHelperTest {
+
+    @InjectMocks
+    private JwtTokenHelper jwtTokenHelper;
+
+    @Mock
+    private UserDetails userDetails;
+
+    @BeforeEach
+    void setUp() {
+        when(userDetails.getUsername()).thenReturn("test");
+    }
+
+    @Test
+    void getUsernameFromToken() {
+        String token = this.jwtTokenHelper.generateToken(userDetails);
+        String usernameFromToken = this.jwtTokenHelper.getUsernameFromToken(token);
+        assertNotNull(usernameFromToken);
+        assertEquals("test", usernameFromToken);
+    }
+
+    @Test
+    void getExpirationDateFromToken() {
+        String token = this.jwtTokenHelper.generateToken(userDetails);
+        Date expirationDateFromToken = this.jwtTokenHelper.getExpirationDateFromToken(token);
+        assertNotNull(expirationDateFromToken);
+        assertTrue(expirationDateFromToken.after(new Date()));
+    }
+
+    @Test
+    void generateToken() {
+        String token = this.jwtTokenHelper.generateToken(userDetails);
+        assertNotNull(token);
+        assertTrue(token.startsWith("eyJ"));
+    }
+
+    @Test
+    void validateToken() {
+        String token = this.jwtTokenHelper.generateToken(userDetails);
+        String usernameFromToken = this.jwtTokenHelper.getUsernameFromToken(token);
+        assertNotNull(usernameFromToken);
+        assertEquals("test", usernameFromToken);
+        assertEquals(userDetails.getUsername(), usernameFromToken);
+        assertTrue(this.jwtTokenHelper.getExpirationDateFromToken(token).after(new Date()));
+    }
+}
