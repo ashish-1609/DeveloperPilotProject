2ad647cc48246301b9b20df2d53c72b7ada1f832
Ashish <ashish@celerispay.com>
2024-09-26 10:57:54 +0530

    Flyway Migration Implemented

--- src/main/java/com/pilot/project/DeveloperPilotProjectApplication.java
+++ src/main/java/com/pilot/project/DeveloperPilotProjectApplication.java
@@ -1,13 +1,20 @@
 package com.pilot.project;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class DeveloperPilotProjectApplication {
 
+    private static Logger logger = LogManager.getLogger(DeveloperPilotProjectApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DeveloperPilotProjectApplication.class, args);
+        logger.info(UUID.randomUUID().toString());
     }
 
 }
